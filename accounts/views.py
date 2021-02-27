43ba03caa245ddd8d237ef0985e144f12f6075b0
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-02-27 21:43:31 +0530

    urls and app geenrated

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home_view(request):
+    return HttpResponse("Home Page")
+
+
+def product_view(request):
+    return HttpResponse("Products Page")
+
+
+def customer_view(request):
+    return HttpResponse("Customer Page")
