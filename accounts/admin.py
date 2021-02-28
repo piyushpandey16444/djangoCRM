72acaa4de0d7ce61422682394951f6a5547d6ac2
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-02-28 16:28:04 +0530

    customer registered in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'date_created', 'write_date')
+    list_display_links = ('name', 'email', 'phone',
+                          'date_created', 'write_date')
+    
--- accounts/models.py
+++ accounts/models.py
@@ -2,13 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    name = models.CharField(string="Customer Name", max_length=255, null=True)
-    phone = models.CharField(max_length=255, null=True)
-    email = models.EmailField(string="Email Id", max_length=254, null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
-    write_date = models.DateTimeField(auto_now=False, null=True)
+    name = models.CharField("Customer Name", max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField("Email Id", max_length=254, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    write_date = models.DateTimeField(auto_now=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-    
