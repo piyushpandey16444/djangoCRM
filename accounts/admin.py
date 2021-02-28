0738d0632e94da0d64df496666d3ce50097d087d
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-02-28 17:19:24 +0530

    admin update

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
 from .models import Customer, Order, Product
+from django.utils.html import format_html
+
 
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone',
+    list_display = ('upper_case_name', 'email', 'phone',
                     'date_created', 'write_date')
-    list_display_links = ('name', 'email', 'phone',
+    list_display_links = ('upper_case_name', 'email', 'phone',
                           'date_created', 'write_date')
     date_hierarchy = 'date_created'
 
-
+    def upper_case_name(self, obj):
+        return f"{(obj.name).upper()}"
+    upper_case_name.short_description = 'Name Given'
 
 
 @admin.register(Product)
@@ -23,7 +27,26 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('status', 'date_created', 'write_date')
-    list_display_links = ('status', 'date_created', 'write_date')
+    list_display = ('colored_status', 'date_created', 'write_date')
+    list_display_links = ('colored_status', 'date_created', 'write_date')
     date_hierarchy = 'date_created'
+
+    def colored_status(self, obj):
+        if obj.status == 'pending':
+            return format_html(
+                '<span style="color: red;">{}</span>',
+                (obj.status).upper(),
+            )
+        elif obj.status == 'out_for_delivery':
+            return format_html(
+                '<span style="color: blue;">{}</span>',
+                (obj.status).upper(),
+            )
+        else:
+            return format_html(
+                '<span style="color: green;">{}</span>',
+                (obj.status).upper(),
+            )
+
+
     
