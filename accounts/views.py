c6b3e48105b4267a7c018cf924c559cfad775ebe
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-01 21:10:45 +0530

    views and templates updated

--- accounts/views.py
+++ accounts/views.py
@@ -4,11 +4,22 @@ from .models import Product, Order, Customer
 
 
 def home_view(request):
+    
     order_objs = Order.objects.all()
     customer_objs = Customer.objects.all()
+    total_customers = customer_objs.count()
+    total_orders = order_objs.count()
+    delivered_orders = order_objs.filter(status="delivered").count()
+    pending_orders = order_objs.filter(status="pending").count()
+
     context = {
         "order_objs": order_objs,
         "customer_objs": customer_objs,
+        "total_customers": total_customers,
+        "total_orders": total_orders,
+        "delivered_orders": delivered_orders,
+        "pending_orders": pending_orders,
+
     }
     return render(request, 'accounts/dashboard.html', context=context)
 
