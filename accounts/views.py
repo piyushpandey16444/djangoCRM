53a93477fbaf7bdc32520fac601f4cd622c486a1
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-01 21:00:34 +0530

    quried for templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Order, Customer
 
 
 def home_view(request):
-    return render(request, 'accounts/dashboard.html')
+    order_objs = Order.objects.all()
+    customer_objs = Customer.objects.all()
+    context = {
+        "order_objs": order_objs,
+        "customer_objs": customer_objs,
+    }
+    return render(request, 'accounts/dashboard.html', context=context)
 
 
 def product_view(request):
